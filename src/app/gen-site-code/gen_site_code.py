37d326a7f2e1cb2fc33d15cf88150d7b1f3f1c25
Fábio Silva <fabio.fernando.osilva@gmail.com>
2024-06-29 12:34:21 -0300

    feat(gen-site-code): adiciona suporte a apostas com mais dezenas

--- src/app/gen-site-code/gen_site_code.py
+++ src/app/gen-site-code/gen_site_code.py
@@ -1,13 +1,19 @@
 import sys
 
 result_file = open(sys.argv[1], "r")
+size_clicks = 0
+
+if len(sys.argv) > 2:
+    size_clicks = int(sys.argv[2])
+
 # header = result_file.readline()
 i = 0
 milliseconds = 0
 for line in result_file:
     print("setTimeout(function () {")
-
-#    print "document.getElementById(\"aumentarnumero\").click();"
+    for i in range(size_clicks):
+        print("document.getElementById(\"aumentarnumero\").click();")
+    
     numeros = map(int, line.strip().split(";"))
     for numero in numeros:
         print("document.getElementById(\"n%02d\").click();" % numero)
